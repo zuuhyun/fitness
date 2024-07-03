92afd88b5f2161771ae4d10d3ab242648a473c4d
lokiki0319@gamil.com <lokiki0319@gmail.com>
2024-07-03 13:50:42 +0900

    first commit

---
+++ src/test/java/com/zuuhyun/PassBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zuuhyun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
